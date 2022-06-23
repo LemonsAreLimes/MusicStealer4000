6d5e4cca8b3c31e1bca278d088b2c5113cf29b3e
LemonsAreLimes <96306499+LemonsAreLimes@users.noreply.github.com>
2022-06-22 21:42:15 -0600

    improved song regognition

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from selenium.webdriver.chrome.service import Service
 from pytube import YouTube
 import os
 import time
-
+import keyboard
 import colorama
 
-PlaylistLink = 'your playlist'
+PlaylistLink = 'your playlist link goes here'
 DownloadDirectory = 'MusicStealer4000\music'
 
 #spotify finder stuff
@@ -19,45 +19,99 @@ artist_parent_class = 'Type__TypeElement-goli3j-0.hHrtFe.rq2VQ5mb9SDAFWbBIUIn.st
 #youtube video, used for getting href
 youtube_link_parent = 'yt-simple-endpoint.style-scope.ytd-video-renderer'
 
-sev = Service("C:\Program Files (x86)\chromedriver_win32(2)\chromedriver.exe")
+#make sure your chomedriver version matches your current chome version
+sev = Service("your chrome driver path")
 op = webdriver.ChromeOptions()
-ms4k = webdriver.Chrome(service=sev, options=op)
+driver = webdriver.Chrome(service=sev, options=op)
 
-ms4k.get(PlaylistLink)
+driver.get(PlaylistLink)
 
-print('scroll down pls')
-time.sleep(10)
-print('okay...')
-
-content_box = ms4k.find_elements(By.CLASS_NAME, content_box_class)
+print('initalized')
+time.sleep(3)
+print(f'{colorama.Fore.GREEN} executing....')
 
 
-    #get the song names and artist
-searchlist = []
-for i in range(len(content_box)):
-    name = content_box[i].find_element(By.CLASS_NAME, name_class)
-    artist = content_box[i].find_element(By.CLASS_NAME, artist_parent_class)
-    searchlist.append([name.text, artist.text])
+
+#get the total number of songs in the playlist
+song_number_str = driver.find_element(By.XPATH, '/html/body/div[3]/div/div[2]/div[3]/div[1]/div[2]/div[2]/div/div/div[2]/main/div/section/div[1]/div[5]/div/span[2]').text.rsplit(" ")
+song_number = int(song_number_str[0])
+
+print(f'{colorama.Fore.RED} CLICK ONTO THE WINDOW!!!!!!!!!!!!!!!')
+print(f'{colorama.Fore.RED} CLICK ONTO THE WINDOW!!!!!!!!!!!!!!!')
+print(f'{colorama.Fore.RED} CLICK ONTO THE WINDOW!!!!!!!!!!!!!!!')
+time.sleep(1)
 
 
-    #goto youtube and download audio
-for song in searchlist:
-    ms4k.get(f'https://www.youtube.com/results?search_query={song}')
 
-    parent_elem = ms4k.find_element(By.CLASS_NAME, youtube_link_parent)
-    link = parent_elem.get_attribute('href')
-    
-            #lil bit of code theifery here but ur no better if u use this >:)
+namelist = []           #all of the song names
+artistlist = []         #all of the artist names
+albumlist = []          #not implomented yet
+coverlist = []          #not implomented yet
+
+#get all of the songs in the playlist
+while True:
+
+    #get all viewable songs
+    content_box = driver.find_elements(By.CLASS_NAME, content_box_class)
+    for elem in range(len(content_box)):
+        try:
+            title = content_box[elem].find_element(By.CLASS_NAME, name_class).text
+            artist = content_box[elem].find_element(By.CLASS_NAME, artist_parent_class).text
+
+            #prevent dupelicates from going on the list
+            if title in namelist:
+                pass
+            else:
+                print(f'{colorama.Fore.BLUE}{len(namelist)}: {colorama.Fore.GREEN}{title}')
+                artistlist.append(artist)
+                namelist.append(title)
+
+        except:
+            print(f'{colorama.Fore.YELLOW}something went wrong.... its ok tho')
+
+    #prevents from endlessly pressing down
+    if len(namelist) == song_number:
+        print(f'{colorama.Fore.MAGENTA}done grabbing songs, time to download!')
+        break
+
+    else:
+        keyboard.press_and_release('down')
+        keyboard.press_and_release('down')
+        keyboard.press_and_release('down')
+        keyboard.press_and_release('down')
+        keyboard.press_and_release('down')
+        keyboard.press_and_release('down')
+
+
+
+#download songs
+for x in range(len(namelist)):
+
+    #parse the link so its valad
+    search_term = namelist[x].replace(" ", "+") + "+" + artistlist[x].replace(" ", "+")
+    page_link = f'https://www.youtube.com/results?search_query={search_term}'
+
+    #goto youtube with song name
+    driver.get(page_link)
+
+    time.sleep(0.1)
+
+    #get the first results link
+    parent_elem = driver.find_element(By.CLASS_NAME, youtube_link_parent)
+    href = parent_elem.get_attribute('href')
+
+
     try:
-        yt = YouTube(link)
+        #i dont know what this does i didnt write it 
+        yt = YouTube(href)
         video = yt.streams.filter(only_audio=True).first()
     
         out_file = video.download(output_path=DownloadDirectory)
         base, ext = os.path.splitext(out_file)
-        new_file = base + '.mp3'
+        new_file = namelist[x] + '.mp3'
 
         os.rename(out_file, new_file)
-        print(yt.title + f'{colorama.Fore.GREEN} has been successfully downloaded.')
+        print(f'{colorama.Fore.GREEN} {yt.title} has been successfully downloaded.')
 
     except FileExistsError:
         print(f'{colorama.Fore.YELLOW}File already exists... moving on')
@@ -65,4 +119,5 @@ for song in searchlist:
     except:
         print(f'{colorama.Fore.RED} yeah boss something went wrong here....')
 
-ms4k.close()
+
+driver.close()
